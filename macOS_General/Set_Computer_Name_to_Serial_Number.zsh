4ef310c3ee9f7cba08b8470ab2575fe2ff113ead
Scripts and Things <ShitttyScripts@gmail.com>
2023-01-16 12:01:30 -0500

    Create Set_Computer_Name_to_Serial_Number.zsh

---
+++ macOS_General/Set_Computer_Name_to_Serial_Number.zsh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+#
+################################################
+#
+# Set_Computer_Name_to_Serial_Number.sh
+# Version: 1.0
+#
+# Sets a Mac's computer name, locahost name, and host name to its serial number.
+# Run with sudo or via MDM.
+#
+################################################
+#
+# Greg Knackstedt
+# ShitttyScripts@gmail.com
+#
+################################################
+#
+jamfBinary="/usr/local/bin/jamf"
+
+computer_name=`ioreg -l | grep IOPlatformSerialNumber|awk '{print $4}' | cut -d " -f 2`
+
+/usr/sbin/scutil --set ComputerName $computer_name
+/usr/sbin/scutil --set LocalHostName $computer_name
+/usr/sbin/scutil --set HostName $computer_name
+
+echo "Status: Naming computer '$computer_name'"
+#
+# Uncomment if using Jamf Pro
+# $jamfBinary setComputerName -useSerialNumber
+
+exit 0
